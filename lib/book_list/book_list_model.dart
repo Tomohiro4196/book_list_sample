ff166ce342ea6c199d6b5560df84693b44c361ee
Tomohiro4196 <tomo4196.w@gmail.com>
2022-02-18 12:54:48 +0900

    prepare to create function add_book

--- lib/book_list/book_list_model.dart
+++ lib/book_list/book_list_model.dart
@@ -4,7 +4,8 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 
 class BookListModel extends ChangeNotifier{
-  final Stream<QuerySnapshot> _usersStream = FirebaseFirestore.instance.collection('books').snapshots();
+  final _userCollection = FirebaseFirestore.instance.collection('books');
+  // _（アンダーバー）を先頭につけると変数がPrivateになる = そのファイル内でしか呼び出せなくなる
   // FirebaseFirestore = firestoreのデータベースを参照
   // .collection = 参照元を指定 → Firestore内のbooksを参照
   //.snapshots でデータを取得
@@ -13,29 +14,31 @@ class BookListModel extends ChangeNotifier{
   List<Book>? books;
   //「?」をつけるとnull許容になる → Book型のデータ or null（=空っぽ）であればOK
 
-  void fetchBookList(){
+  void fetchBookList() async {
     //▼変化をListenするコード
-    _usersStream.listen((QuerySnapshot snapshot) {
-      final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
-        //List<Book> books = 「Book」型のレコードから成る「books」というリスト
-        //mapでDocumentSnapshotをBookに変換する
-        Map<String, dynamic>
-        data = document.data()! as Map<String, dynamic>;
-        final String title = data['title'];
-        final String author = data['author'];
-        return Book(title, author);
-        //Bookという型にしてMapの中で変換する -> Bookから成るbooksというリストになる
-      }).toList();
-      //toList -> リストに追加する
-      this.books = books;
-      //this = 現在のインスタンスを指す
-      //this.books = 現在のインスタンスのbookデータを参照する
-      notifyListeners();
-      //通知を送るメソッド
-      //book_list_pageで発火
-    });
 
+    final QuerySnapshot snapshot = await _userCollection.get();
+    // QuerySnapshot snapshot = QuerySnapshotという種類のsnapshotを指定している
+    // getでデータを取得 = getなので、リアルタイムで反映されない
+    final List<Book> books = snapshot.docs.map((DocumentSnapshot document) {
+      //List<Book> books = 「Book」型のレコードから成る「books」というリスト
+      //mapでDocumentSnapshotをBookに変換する
 
+      //Booksのリストを作成
+      Map<String, dynamic>
+      data = document.data()! as Map<String, dynamic>;
+      final String title = data['title'];
+      final String author = data['author'];
+      return Book(title, author);
+      //Bookという型にしてMapの中で変換する -> Bookから成るbooksというリストになる
+    }).toList();
 
+    //toList -> リストに追加する
+    this.books = books;
+    //this = 現在のインスタンスを指す
+    //this.books = 現在のインスタンスのbookデータを参照する
+    notifyListeners();
+    //通知を送るメソッド
+    //book_list_pageで発火
+    }
   }
-}
